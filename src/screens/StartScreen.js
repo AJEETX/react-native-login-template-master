ea841b0eb623bc9cf80951f7299ce32431446ce7
ajeet <ajeetx@email.com>
2023-06-01 01:30:18 +1000

    add logo

--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -9,7 +9,7 @@ export default function StartScreen({ navigation }) {
   return (
     <Background>
       <Logo />
-      <Header>Login Agent</Header>
+      <Header>risk.control.unit</Header>
       <Paragraph>
         The easiest way to start with your amazing application.
       </Paragraph>
